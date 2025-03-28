de59c2c11425d0ecf715544c91556aab39254c65
jonnyjavi <jonathanjavier.calvasoto@gmail.com>
2025-03-28 17:51:34 +0100

    Version 2

--- testGit.java
+++ testGit.java
@@ -1,5 +1,6 @@
 public class testGit {
     public static void main(String[] args ) {
-	System.out.println("Hola");
+	System.out.println("testGit");
+	System.out.println("Jonathan Javier Calva Soto");
     }
 }
